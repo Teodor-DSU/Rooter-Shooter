4ac356ebeaa915743ef575f450dd531d451dccea
a19teosc <60491220+Teodor-DSU@users.noreply.github.com>
2023-02-04 09:55:41 +0100

    New scene folder
    You can actually see it now

---
+++ GGJ Rooter Shooter/Assets/Scripts/SeedTravel.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeedTravel : MonoBehaviour
+{
+    [SerializeField] private float speed = 5f;
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+        transform.Translate(new Vector3());
+    }
+}
