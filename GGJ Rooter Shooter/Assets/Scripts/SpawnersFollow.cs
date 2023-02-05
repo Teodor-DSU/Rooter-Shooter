d0631d0f4aa2479cd76d8c61e583120afefc9a3e
VMormoris <vmormoris2014@gmail.com>
2023-02-05 11:57:38 +0100

    Made spawners follow player

---
+++ GGJ Rooter Shooter/Assets/Scripts/SpawnersFollow.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpwnFollow : MonoBehaviour
+{
+    private float offsetX;
+    // Start is called before the first frame update
+    void Start()
+    {
+        offsetX = transform.position.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Transform target = PlayerController.ActivePlayer;
+        transform.position = new Vector3(target.position.x + offsetX, transform.position.y, 0.0f);
+    }
+}
