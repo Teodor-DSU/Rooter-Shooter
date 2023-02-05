f46ad345d80eb388df723791241f207530cac48f
a19teosc <60491220+Teodor-DSU@users.noreply.github.com>
2023-02-05 10:47:18 +0100

    DyingTimer but in wronf script

---
+++ GGJ Rooter Shooter/Assets/Scripts/SceneBehaviour.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class SceneBehaviour : MonoBehaviour
+{
+      [SerializeField] private VoidEventChannelSO startedDying; 
+      [SerializeField] private VoidEventChannelSO gotNewHost;
+      [SerializeField] private int secondsTilDie = 5;
+      [SerializeField] private GameObject textObject;
+
+      private TMP_Text text;
+      private int currentTime;
+
+      private void Start()
+      {
+            text = textObject.GetComponent<TMP_Text>();
+            textObject.SetActive(false);
+      }
+
+      private void StartCountDown()
+      {
+            textObject.SetActive(true);
+            currentTime = secondsTilDie;
+            text.text = currentTime.ToString();
+      }
+
+      private void StopCountDown()
+      {
+            
+      }
+
+      private IEnumerator CountDown()
+      {
+            while (currentTime > 0)
+            {
+                  yield return new WaitForSeconds(1);
+                  currentTime--;
+                  text.text = currentTime.ToString();
+            }
+      }
+}
