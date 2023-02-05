2d74eac42d7cd0d29721c0efb6dc0a3031563035
a19teosc <60491220+Teodor-DSU@users.noreply.github.com>
2023-02-05 10:50:53 +0100

    Correct dyin script and scene behaviour

--- GGJ Rooter Shooter/Assets/Scripts/DyingTimer.cs
+++ GGJ Rooter Shooter/Assets/Scripts/DyingTimer.cs
@@ -1,18 +1,58 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class DyingTimer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private VoidEventChannelSO startedDying; 
+    [SerializeField] private VoidEventChannelSO gotNewHost;
+    [SerializeField] private int secondsTilDie = 5;
+    [SerializeField] private GameObject textObject;
+
+    private TMP_Text text;
+    private int currentTime;
+
+    private void Start()
     {
-        
+        text = textObject.GetComponent<TMP_Text>();
+        textObject.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+        startedDying.OnEventRaised += StartCountDown;
+        gotNewHost.OnEventRaised += StopCountDown;
+    }
+    
+    private void OnDisable()
+    {
+        startedDying.OnEventRaised -= StartCountDown;
+        gotNewHost.OnEventRaised -= StopCountDown;
+    }
+
+    private void StartCountDown()
+    {
+        textObject.SetActive(true);
+        currentTime = secondsTilDie;
+        text.text = currentTime.ToString();
+        StartCoroutine(CountDown());
+    }
+
+    private void StopCountDown()
+    {
+        textObject.SetActive(false);
+        StopCoroutine(CountDown());
+    }
+
+    private IEnumerator CountDown()
+    {
+        while (currentTime > 0)
+        {
+            yield return new WaitForSeconds(1);
+            currentTime--;
+            text.text = currentTime.ToString();
+        }
     }
 }
--- GGJ Rooter Shooter/Assets/Scripts/SceneBehaviour.cs
+++ GGJ Rooter Shooter/Assets/Scripts/SceneBehaviour.cs
@@ -4,42 +4,17 @@ using System.Collections.Generic;
 using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneBehaviour : MonoBehaviour
 {
-      [SerializeField] private VoidEventChannelSO startedDying; 
-      [SerializeField] private VoidEventChannelSO gotNewHost;
-      [SerializeField] private int secondsTilDie = 5;
-      [SerializeField] private GameObject textObject;
+    private void LoadScene(string scenename)
+    {
+        SceneManager.LoadScene(scenename);
+    }
 
-      private TMP_Text text;
-      private int currentTime;
-
-      private void Start()
-      {
-            text = textObject.GetComponent<TMP_Text>();
-            textObject.SetActive(false);
-      }
-
-      private void StartCountDown()
-      {
-            textObject.SetActive(true);
-            currentTime = secondsTilDie;
-            text.text = currentTime.ToString();
-      }
-
-      private void StopCountDown()
-      {
-            
-      }
-
-      private IEnumerator CountDown()
-      {
-            while (currentTime > 0)
-            {
-                  yield return new WaitForSeconds(1);
-                  currentTime--;
-                  text.text = currentTime.ToString();
-            }
-      }
+    private void QuitGame()
+    {
+        Application.Quit();
+    }
 }
