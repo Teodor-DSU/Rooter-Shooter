595545cdb48e756e4b3ca5d659959970e0bef492
VMormoris <vmormoris2014@gmail.com>
2023-02-05 12:09:19 +0100

    Make alien spawners also follow player

--- GGJ Rooter Shooter/Assets/Scripts/SpawnersFollow.cs
+++ GGJ Rooter Shooter/Assets/Scripts/SpawnersFollow.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpwnFollow : MonoBehaviour
+public class SpawnersFollow : MonoBehaviour
 {
     private float offsetX;
     // Start is called before the first frame update
